71f8ced5cd8027621e5917bc4c34869a9a8d59e9
Stefano Zantedeschi <st.zantedeschi@gmail.com>
2022-01-29 18:58:33 +0100

    data cleaning description for RegularMedicine

--- dashboard_functions.py
+++ dashboard_functions.py
@@ -20,6 +20,18 @@ def replacing_null_values(df_diabetes):
 
     return df_diabetes
 
+# functions to describe how the null values were replaced in the 4 columns that presented that type of value
+def text_for_nan_cleaning(nan_column):
+    if nan_column == 'BMI':
+        return st.write('Since the distribution of the values of the column called ' + nan_column + 
+                    ' is not a normal distribution, the null values were substituted with the mode value of the column.')
+    if (nan_column == 'Pregnancies') | (nan_column == 'Pdiabetes'):
+        return st.write('Since, the selected column can be considered as a categorical column with 5 (from 0 to 4) values,' + 
+                    'the null values were substitued with the most frequent value. Before doing that, it was necessary to change the type of the column.')
+    if nan_column == 'Diabetic':
+        return st.write('Since, one goal of the project presented here is to predict if a person has diabetes or not, the selected column is the target of the project. ' + 
+                    'So, the row that presented the only null value of the ' + nan_column + ' column has been removed from the dataset.')
+        
 # function to clean errors in columns RegularMedicine, BPLevel, Pdiabetes and Diabetic
 def datacleaning(df_diabetes):
     # In the column RegularMedicine there is one field valued with 'o'. The column must have values 'yes/no'.
@@ -39,20 +51,5 @@ def datacleaning(df_diabetes):
     df_diabetes.Diabetic = df_diabetes.Diabetic.str.replace(' ', '')
 
     return df_diabetes
-
-# functions to describe how the null values were replaced in the 4 columns that presented that type of value
-def text_for_nan_cleaning(nan_column):
-    if nan_column == 'BMI':
-        return st.write('Since the distribution of the values of the column called ' + nan_column + 
-                    ' is not a normal distribution, the null values were substituted with the mode value of the column.')
-    if (nan_column == 'Pregnancies') | (nan_column == 'Pdiabetes'):
-        return st.write('Since, the selected column can be considered as a categorical column with 5 (from 0 to 4) values,' + 
-                    'the null values were substitued with the most frequent value. Before doing that, it was necessary to change the type of the column.')
-    if nan_column == 'Diabetic':
-        return st.write('Since, one goal of the project presented here is to predict if a person has diabetes or not, the selected column is the target of the project. ' + 
-                    'So, the row that presented the only null value of the ' + nan_column + ' column has been removed from the dataset.')
-
-        
-    
 
     
