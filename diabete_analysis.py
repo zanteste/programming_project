51680dce9a1a00f69e6b1f35f248567b3cd13fcf
Stefano Zantedeschi <st.zantedeschi@gmail.com>
2022-01-29 17:18:06 +0100

    null values data cleaning

--- diabete_analysis.py
+++ diabete_analysis.py
@@ -1,6 +1,9 @@
+from cmath import nan
+from tkinter import W
 import pandas as pd
 import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt
 
 # settings for streamlit page
 st.set_page_config(page_title=None, page_icon=None, layout="wide", initial_sidebar_state="auto")
@@ -19,16 +22,56 @@ st.write(
     'The data used here can be found on Kaggle, at the following link: \n _https://www.kaggle.com/tigganeha4/diabetes-dataset-2019_. ' +
     'The dataset about diabetes is composed by ' + str(df_diabetes.shape[0]) + ' rows and ' + str(df_diabetes.shape[1]) + ' columns.')
 
-# all dataset columns are saved into a list
-columns_list = columns_list = df_diabetes.columns.to_list()
-
+# all dataset columns in a list
+columns_list = df_diabetes.columns.to_list()
 # all columns except the last one are merged in a string
 all_columns_except_last = ', '.join(columns_list[:-1])
 
+st.header('Data Exploration and Data Cleaning')
 # in the following text (st.write) the name of each column is shown
 st.write(
-    'The columns of the diabetes dataset are the following: *' + all_columns_except_last + '* and *' + columns_list[len(columns_list)-1] + '*.'
+    'The columns of the diabetes dataset are the following: *' + all_columns_except_last + '* and *' + columns_list[len(columns_list)-1] + '*.' +
+    "It's possible to see that two columns have some spelling errors. They have been corrected as follows: "
 )
+st.markdown("- *Pregancies* became *Pregnancies*; \n - *UriationFreq* became *UrinationFreq*.")
+# renaming of Pregancies and UriationFreq
+df_diabetes.rename(columns={'Pregancies':'Pregnancies', 'UriationFreq':'UrinationFreq'}, inplace=True)
+# dataset columns in list after renaming the two columns before
+columns_list = df_diabetes.columns.to_list()
+
+# list of the dataset columns with null values inside
+columns_with_nan = df_diabetes.columns[df_diabetes.isna().any()].to_list()
+# columns with nan to an unique str, in order to concatenate with the following text
+string_columns_with_nan = ", ".join(columns_with_nan)
+st.write("While browsing the data, it has been discovered that there were some null values in the original dataset." + 
+" In particular the null values were in the following columns: " + string_columns_with_nan + ". " + 
+" \n If you are interested in the cleaning of the null values click the box below.")
+
+with st.beta_expander('Get info about cleaning of null values'):
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        choosen_column_with_nan = st.selectbox('', columns_with_nan)
+    with col2:
+        for nan_col in columns_with_nan:
+            if nan_col == choosen_column_with_nan:
+                number_of_nan_values= df_diabetes[nan_col].isnull().sum()
+                st.write(' \n There were ' + str(number_of_nan_values) + ' null values in the selected column.')
+                if nan_col == 'BMI':
+                    st.write('Since the distribution of the values of the column called ' + nan_col + 
+                    ' is not a normal distribution, the null values were substituted with the mode value of the column.')
+                    if st.button('Show ' + nan_col + ' distribution'):
+                        fig, ax = plt.subplots(1,1, figsize=(2,2))
+                        df_diabetes[nan_col].hist(ax=ax)
+                        st.pyplot(fig)
+                if (nan_col == 'Pregnancies') | (nan_col == 'Pdiabetes'):
+                    st.write('Since, the selected columns can be considered as a categorical column with 5 (from 0 to 4) values,' + 
+                    'the null values were substitued with the most frequent value. Before doing that, it was necessary to change the type of the column.')
+                    most_frequent_value = df_diabetes[nan_col].mode()[0]
+                    st.write('The most frequent value in the selected columns is ' + str(most_frequent_value) + ". So all the nan values have been filled with it.") 
+                if nan_col == 'Diabetic':
+                    st.write('Since, one goal of the project presented here is to predict if a person has diabetes or not, the selected column is the target of the project. ' + 
+                    'So, the row that presented the only null value of the ' + nan_col + ' column has been removed from the dataset.')
+                
 
 
 # dictionary contain the meaning of each columns
@@ -39,9 +82,10 @@ meaning_of_columns = {'Age':'Age of the participants', 'Gender': 'Sex of the par
                        'Sleep':'It indicates how much a participant sleeps', 'Soundsleep':'It indicates the amount of hours of sound sleep', 
                        'RegularMedicine':'It indicates if a participant takes medicines regularly or not',
                        'JunkFood':'It indicates if a participant consumes a junk food or not','Stress':'It indicates the level of stress of the participants', 
-                       'BPLevel':'Blood pressure level of participants', 'Pregancies':'Number of pregnanancis for each participant', 
+                       'BPLevel':'Blood pressure level of participants', 'Pregnancies':'Number of pregnanancis for each participant', 
                        'Pdiabetes':'Gestation diabetes', 
-                       'UriationFreq': 'Frequency of urination', 'Diabetic':'It indicates if a participant is diabetic or not'}
+                       'UrinationFreq': 'Urination frequency', 'Diabetic':'It indicates if a participant is diabetic or not'}
+
 
 # creation of two columns in order to have the selectbox and the meaning of the selected column side by side
 
