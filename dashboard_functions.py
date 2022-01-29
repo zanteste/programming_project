8a23326e656235c3230982495b2ca9c8d38e9b06
Stefano Zantedeschi <st.zantedeschi@gmail.com>
2022-01-29 18:21:14 +0100

    add function for replacing null values

--- dashboard_functions.py
+++ dashboard_functions.py
@@ -1,6 +1,25 @@
 import pandas as pd 
 import streamlit as st
 
+
+def replacing_null_values(df_diabetes):
+    # replace null values in the column BMI with the mode value
+    BMI_mode_value = df_diabetes.BMI.mode()[0]
+    df_diabetes.BMI = df_diabetes.BMI.fillna(BMI_mode_value)
+
+    # replace null values in the column Pregnancies with the most frequent value
+    Pregnancies_most_frequent_value = df_diabetes.Pregnancies.value_counts().index.to_list()[0]
+    df_diabetes.Pregnancies = df_diabetes.Pregnancies.fillna(Pregnancies_most_frequent_value)
+
+    # replace null values in the column Pdiabetes with the most frequent value
+    Pdiabetes_most_frequent_value = df_diabetes.Pdiabetes.value_counts().index.to_list()[0]
+    df_diabetes.Pdiabetes = df_diabetes.Pdiabetes.fillna(Pdiabetes_most_frequent_value)
+
+    # remove the row that has nan value in the column Diabetic
+    df_diabetes = df_diabetes[df_diabetes.Diabetic.notnull()]
+
+
+# functions to describe how the null values were replaced in the 4 columns that presented that type of value
 def text_for_nan_cleaning(nan_column):
     if nan_column == 'BMI':
         return st.write('Since the distribution of the values of the column called ' + nan_column + 
