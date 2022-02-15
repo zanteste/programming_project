d775e02a2c5d7512b601f50d9b2ab9be2ee71a5e
Stefano Zantedeschi <st.zantedeschi@gmail.com>
2022-02-15 16:56:50 +0100

    adding introduction page

--- diabetes_prediction_dashboard.py
+++ diabetes_prediction_dashboard.py
@@ -7,14 +7,16 @@ import streamlit as st
 import data_exploration
 import data_analysis
 import machinelearning_algorithm
+import introduction
 
 pages = {
+    "Introduction": introduction, 
     "Data Exploration": data_exploration,
     "Data Analysis": data_analysis,
-    "Diabetes Prediction": machinelearning_algorithm
+    "Machine Learning Algorithms": machinelearning_algorithm
 }
 
-st.sidebar.title('Menu')
+st.sidebar.title('Diabetes Prediction App')
 selection = st.sidebar.radio("Go to", list(pages.keys()))
 page = pages[selection]
 page.app()
---
+++ introduction.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+
+def app():
+    st.title('Diabetes Prediction')
+
+    st.header('Introduction')
+    
+    st.write("The work presented in this application has the goal to analyze the main causes of diabetes and to predict if a person has " +
+            "diabetes or not. The data used here can be found on Kaggle at the following link: https://www.kaggle.com/tigganeha4/diabetes-dataset-2019.")
+
+    st.write("The application is composed by three different pages thant can be reach from the menu in the sidebar: ")
+    st.markdown('- **Data Exploration**, in which the dataset is explored and data cleaning operations are described.')
+    st.markdown("- **Data Analysis**, in which the features of the dataset are analyzed to find some correlations between them and to find " +
+                " the main causes of diabetes.")
+    st.markdown("- **Diabetes Prediction**, in which the original dataset is modified in order to be ready for machine learning algorithms and " +
+                "the result of three different algorithms trained on the available data are shown.")
+
+    
