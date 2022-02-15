4b41d5c9083ef4ec7b927528abb181e26ed006fc
Stefano Zantedeschi <st.zantedeschi@gmail.com>
2022-02-15 17:06:28 +0100

    solving error import in main file

--- diabetes_prediction_dashboard.py
+++ diabetes_prediction_dashboard.py
@@ -5,15 +5,15 @@ from re import S
 import streamlit as st
 
 #import data_exploration
-import machinelearning_algorithm
 import introduction
 import Data_Exploration.data_exploration as exploration
 import Data_Analysis.data_analysis as analysis
+import Machine_Learning.machinelearning_algorithm as machinelearning
 pages = {
     "Introduction": introduction, 
     "Data Exploration": exploration,
     "Data Analysis": analysis,
-    "Machine Learning Algorithms": machinelearning_algorithm
+    "Machine Learning Algorithms": machinelearning
 }
 
 st.sidebar.title('Diabetes Prediction App')
